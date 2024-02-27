a1f3029f5891bf22ff16ddedc00ec9a15746101f
CarmineN00 <81189220+CarmineN00@users.noreply.github.com>
2024-02-27 16:47:49 +0100

    Changed the model converter

--- get_dot_h.py
+++ get_dot_h.py
@@ -2,17 +2,25 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
 from keras.models import load_model
-from tinymlgen import port
 from utility import get_dataset_path, get_dataframe_split
+from everywhereml.code_generators.tensorflow import convert_model
 
-'''pathDSTrain = get_dataset_path("TrainSHAPSNNImportantFeatures")
+pathDSTrain = get_dataset_path("TrainSHAPSNNImportantFeatures")
 pathDSTest = get_dataset_path("TestSHAPSNNImportantFeatures")
 
 x_train , y_train = get_dataframe_split(pathDSTrain)
 x_test , y_test = get_dataframe_split(pathDSTest)
 
-#avoid the custom layer problem
-model = Sequential()
+#one hot encode labels to adapt with model converter
+'''x_test = np.asarray(x_test)
+y_test = np.asarray(y_test)
+
+y_test = np.zeros((len(y_test), 3), dtype=int)
+for i in range(len(y_test)):
+    y_test[i, y_test[i]] = 1'''
+
+#avoid the custom layer problem snn
+'''model = Sequential()
 model.add(Dense(41, input_dim=x_train.shape[1], activation='relu'))
 model.add(Dense(20, activation='relu'))
 model.add(Dense(units=3, activation="softmax")) 
@@ -20,9 +28,8 @@ model.compile(loss="sparse_categorical_crossentropy", optimizer='adam', metrics=
 model.fit(x_train, y_train, epochs=5, batch_size=32, verbose=0)
 model.save("TrainedModels\\SHAPSNNImportantFeatures.h5")'''
 
-#convert a trained model in .h module
+#convert a snn trained model in .h module
 snn = load_model("TrainedModels\\SHAPSNNImportantFeatures.h5")
-c_code = port(snn, variable_name="snn_model", pretty_print=True)
-print(c_code)
-
+c_header = convert_model(snn, x_test, y_test, model_name='snn_model')
+print(c_header)
 
